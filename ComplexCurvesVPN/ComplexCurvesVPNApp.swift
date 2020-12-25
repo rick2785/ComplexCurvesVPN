bf273e42b050240492ff68e76e476770c1c10ed1
rick2785 <rjhrabowskie@gmail.com>
2020-12-25 10:57:31 -0600

    Initial Commit

---
+++ ComplexCurvesVPN/ComplexCurvesVPNApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexCurvesVPNApp.swift
+//  ComplexCurvesVPN
+//
+//  Created by RJ Hrabowskie on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct ComplexCurvesVPNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComplexCurvesVPN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ComplexCurvesVPN
+//
+//  Created by RJ Hrabowskie on 12/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
